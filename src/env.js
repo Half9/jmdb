47393538ad151b372531f2d98aaff18f44892c08
Half9 <3681520+Half9@users.noreply.github.com>
2022-07-21 11:15:09 +0200

    Delete env.js

--- src/env.js
+++
@@ -1,6 +0,0 @@
-export default {
-  API_KEY: "ee3d14f80b2524ff51afc964ff68ff9f",
-  BASE_URL: "https://api.themoviedb.org/3/",
-  IMG_URL: "https://image.tmdb.org/t/p/w500",
-  NETLIFY_URL: "http://localhost:8888/",
-};
